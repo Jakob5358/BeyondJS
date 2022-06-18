747a5de2542eda3e8eec307d21008e7d736cbe72
Jakob <75086936+Jakob5358@users.noreply.github.com>
2022-06-18 11:41:25 +0200

    Use new deno 1.23 features and export stuff

--- mod.ts
+++ mod.ts
@@ -1,3 +1,6 @@
 export * from './structs/mod.ts';
 export * from './types/mod.ts';
 export * from './decorators/mod.ts';
+export * from './decorators/mod.ts';
+export * from './structs/mod.ts';
+export * from './types/mod.ts';
